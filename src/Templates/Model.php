244f15e0f2f338a0b3491b24718960efbcd4f52a
Juri Salminen <juri@spix.nu>
2020-12-06 11:09:42 +0100

    Added template for Models.

---
+++ src/Templates/Model.php
@@ -0,0 +1,61 @@
+<?php
+$class = "<?php
+
+
+namespace Gutenisse\PhpOrmLite\Models;
+
+use Exception;
+use Gutenisse\PhpOrmLite\Model;
+
+class {$this->getUcaseFirst($this->getName())}
+{\n";
+$class .= "\tprivate static string \$_tableName = \"{$this->getName()}\";\n";
+
+$class .= "
+    /**
+    * @return Products[]
+    * @throws Exception
+    */
+    public static function getAll() : array
+    {
+        try {
+            \$products = Model::list(self::\$_tableName);
+        } catch (Exception \$exception) {
+            throw \$exception;
+        }
+        
+        return \$products;
+    }\n\n";
+
+$class .= "
+    /**
+    * @param  int \$id
+    *
+    * @return Products
+    * @throws Exception
+    */
+    public static function getById(int \$id) : {$this->getName()}
+    {
+        try {
+            return Model::get(\$id, self::\$_tableName);
+        } catch (Exception \$exception) {
+            throw \$exception;
+        }
+    }
+";
+
+foreach ($this->getColumns() as $column) {
+    $class .= "\tprivate {$this->getType($column)} \${$this->getLowercase($column->getName())};\n";
+}
+
+$class .= "\n";
+
+foreach ($this->getColumns() as $column) {
+    $type = $column->isNumeric() ? "int" : "string";
+    $class .= "\tpublic function get{$this->getUcaseFirst($column->getName())}() : {$type}\n";
+    $class .= "\t{\n";
+    $class .= "\t\treturn \$this->{$this->getLowercase($column->getName())};\n";
+    $class .= "\t}\n\n";
+}
+
+$class .= "}";
