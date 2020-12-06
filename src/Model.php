af95860c3fc60dc84157c641a7265490eacd15ff
Juri Salminen <juri@spix.nu>
2020-12-06 11:24:48 +0100

    Added get-function to Model.php

--- src/Model.php
+++ src/Model.php
@@ -27,4 +27,22 @@ class Model
         
         return $entities;
     }
+    
+    /**
+     * @param  int  $id
+     * @param  string  $table
+     *
+     * @return mixed
+     * @throws Exception
+     */
+    public static function get(int $id, string $table) : mixed
+    {
+        $stmt = PhpOrmLite::getDc()->getPdo()->prepare("SELECT * FROM {$table} WHERE id = :id;");
+        $stmt->bindValue(":id", $id, PDO::PARAM_INT);
+        $stmt->execute();
+    
+        if($stmt->rowCount() === 0) throw new Exception("The table '{$table}' does not contain a row with 'id' {$id}.");
+        
+        return $stmt->fetchAll(PDO::FETCH_CLASS, "Gutenisse\\PhpOrmLite\\Models\\". ucfirst($table))[0];
+    }
 }
